a91c4933211025d31d19964e7524b083d9c7bab6
cristopher <cr.ignacio2003@gmail.com>
2026-04-16 13:07:14 -0400

    cambios

--- src/empleado_dispositivo/empleado_dispositivo.controller.ts
+++ src/empleado_dispositivo/empleado_dispositivo.controller.ts
@@ -20,4 +20,9 @@ export class EmpleadoDispositivoController {
     return this.empleadoDispositivoService.findAll(+page, +limit);
   }
 
+  @Get(':id')
+  buscarPorEmpleado(@Param('id') id: string) {
+    return this.empleadoDispositivoService.buscarPorEmpleado(+id);
+  }
+
 }
